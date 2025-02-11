cab9440d8e83b7fc35c62c980be07fc776ae00e7
Your Name <your.email@example.com>
2025-02-11 15:42:25 +0530

    renamed:    Views/Home/index.js -> Views/Home/index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import ‘./App.css’
-import home
-import header
-import { BrowserRouter, Route, Routes } from ‘react - router - dom’
+import './App.css'
+import './Views/Home'
+import './Components/Header'
+import { BrowserRouter, Route, Routes } from 'react - router - dom';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <Header/>
       <Routes>
-        <Route path=”/” exact={true} element=
-        {<Home />}/>
+        <Route path="/" exact={true} element={<Home />}/>
       </Routes>
     </BrowserRouter>
   )
