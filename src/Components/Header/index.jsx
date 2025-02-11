f1cdcd1554380358b433c31a822b011fadb48c43
Your Name <your.email@example.com>
2025-02-11 16:38:10 +0530

    Save all changes in App.css, omponents/Header/index.jsx

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,7 +1,13 @@
 export default function Header() {
   return (
-    <div>
-      Header
+    <div className="headerRow">
+      <div className="topRow bg-blue">
+        <p className="mt-0 mb-0 text-center" >
+          Duet to the <b>COVID 19</b>epidemic, others may be processed with a slight delay
+        </p>
+
+      </div>
+      
     </div>
   )
 }
