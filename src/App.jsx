e2ee149bd4e1326b5ad70047e986b886879fe8cc
Your Name <your.email@example.com>
2025-02-25 15:58:46 +0530

    Save all changes and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
-import './Views/Home'
-import './Components/Header'
-import { BrowserRouter, Route, Routes } from 'react - router - dom';
+import Home from './Pages/Home/index'
+import Header from './Components/Header/index'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 export default function App() {
   return (
+    <div className='app-container mx-auto'>
     <BrowserRouter>
       <Header/>
       <Routes>
         <Route path="/" exact={true} element={<Home />}/>
       </Routes>
     </BrowserRouter>
+    </div>
   )
 }
 
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,13 +1,59 @@
-export default function Header() {
-  return (
-    <div className="headerRow">
-      <div className="topRow bg-blue">
-        <p className="mt-0 mb-0 text-center" >
-          Duet to the <b>COVID 19</b>epidemic, others may be processed with a slight delay
-        </p>
+import React from 'react'
+import Logo from '../../assets/images/logo.webp'
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom'
+import { IoSearch } from "react-icons/io5";
+import { LuUserRound } from "react-icons/lu";
+import { IoBagOutline } from "react-icons/io5";
+import CountryDrop from '../CountryDrop';
 
+
+const Header = () => {
+  return (
+    <div>
+      <div className='headerRow'>
+        <div className='topRow bg-blue'>
+          <div className="container">
+            <p className="mb-0 mt-0 text-center">
+              Due to the <strong>COVID 19</strong> epidemic, orders may be processed with a slight delay
+            </p>
+          </div>
+        </div>
+        {/* Day-3 ... Creating second Row  */}
+        <header className='header'>
+          <div className='container'>
+            <div className='row'>
+
+              <div className='logoCompany d-flex align-items-center col-sm-2'>
+                <Link to={'/'}> <img src={Logo} alt="Company Image" /></Link>
+              </div>
+
+              <div className='col-sm-10 d-flex align-items-center part-2'>
+                <CountryDrop />
+                {/* Day-4 Work Search BOX */}
+                <div className='headerSearch ms-3 me-3'>
+                  <input type="text" placeholder='Search for products...' />
+                  <Button><IoSearch /></Button>
+                </div>
+
+                <div className='part-3 d-flex align-items-center ml-auto'>
+                  <Button className='circle me-3'><LuUserRound /></Button>
+                  <div className='cart d-flex align-items-center'>
+                    <span className='price'>$10.00</span>
+                    <div className='position-relative ms-2'>
+                      <Button className='circle'><IoBagOutline /></Button>
+                      <span className='count d-flex align-items-center justify-content-center'>3</span>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            </div>
+        </header>
       </div>
-      
     </div>
   )
 }
+
+export default Header;
